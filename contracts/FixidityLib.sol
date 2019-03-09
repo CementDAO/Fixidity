7a2d94e8e670a889a2690641a973a2d4b07cee86
Alberto Cuesta <alberto@techhq.io>
2019-03-09 21:23:59 +0000

    Added Fixidity library

--- contracts/FixidityLib.sol
+++ contracts/FixidityLib.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.24;
+pragma solidity ^0.5.0;
 
 library FixidityLib {
 
@@ -82,6 +82,7 @@ library FixidityLib {
 
     function trunc_digits(Fixidity storage fixidity, int256 v, uint8 digits) public view returns (int256) {
         if(digits <= 0) return v;
-        return round_off(fixidity, v, digits) / (10 ** digits);
+        return round_off(fixidity, v, digits)
+            / int256(uint256(10) ** uint256(digits));
     }
 }
