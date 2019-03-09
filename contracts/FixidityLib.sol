a6fc10b7831989c7ab8b8955327ad2ce0bbffa43
Alberto Cuesta <alberto@techhq.io>
2019-03-09 21:28:01 +0000

    Removed Fixidity struct from FixidityLib and made all functions pure, also removed support for digits other than 36.

--- contracts/FixidityLib.sol
+++ contracts/FixidityLib.sol
@@ -2,55 +2,37 @@ pragma solidity ^0.5.0;
 
 library FixidityLib {
 
-    uint8 constant public initial_digits = 36;
+    uint8 constant public digits = 36;
+    int256 constant public fixed_1 =            1000000000000000000000000000000000000;
     int256 constant public fixed_e =            2718281828459045235360287471352662498;
     int256 constant public fixed_pi =           3141592653589793238462643383279502884;
     int256 constant public fixed_exp_10 =   22026465794806716516957900645284244000000;
     int256 constant public max_int256 =     57896044618658097711785492504343953926633;
 
-    struct Fixidity 
-    {
-        uint8 digits;
-        int256 fixed_1;
-        int256 fixed_e;
-        int256 fixed_pi;
-        int256 fixed_exp_10;
-    }
-
-    function init(Fixidity storage fixidity, uint8 digits) public {
-        assert(digits <= 36);
-        fixidity.digits = digits;
-        fixidity.fixed_1 = int256(uint256(10) ** uint256(digits));
-        int256 t = int256(uint256(10) ** uint256(initial_digits - digits));
-        fixidity.fixed_e = fixed_e / t;
-        fixidity.fixed_pi = fixed_pi / t;
-        fixidity.fixed_exp_10 = fixed_exp_10 / t;
-    }
-
     /**
      * @dev Converts an int256 to fixidity units, equivalent to multiplying
      * by 10^6.
      */
-    function newFromInt256(Fixidity storage fixidity, int256 x)
+    function newFromInt256(int256 x)
         public
-        view
+        pure
         returns (int256)
     {
         assert(x < max_int256); // Cannot process numbers above 57896044618658097711785492504343953926633.
-        return multiply(fixidity, x,fixidity.fixed_1);
+        return multiply(x, fixed_1);
     }
 
     /**
      * @dev Converts an uint256 to fixidity units, equivalent to multiplying
      * by 10^6.
      */
-    function newFromUint256(Fixidity storage fixidity, uint256 x)
+    function newFromUint256(uint256 x)
         public
-        view
+        pure
         returns (int256)
     {
         assert(x < uint256(max_int256)); // Cannot process numbers above 57896044618658097711785492504343953926633.
-        return multiply(fixidity, int256(x),fixidity.fixed_1);
+        return multiply(int256(x), fixed_1);
     }
 
     /**
@@ -58,19 +40,18 @@ library FixidityLib {
      * equivalent to multiplying by 10^6.
      */
     function newFromInt256Fraction(
-        Fixidity storage fixidity, 
         int256 numerator, 
         int256 denominator
         )
         public
-        view
+        pure
         returns (int256)
     {
         assert(numerator < max_int256);   // Cannot process numbers above 57896044618658097711785492504343953926633.
         assert(denominator < max_int256); // Cannot process numbers above 57896044618658097711785492504343953926633.
-        int256 convertedNumerator = newFromInt256(fixidity, numerator);
-        int256 convertedDenominator = newFromInt256(fixidity, denominator);
-        return divide(fixidity, convertedNumerator, convertedDenominator);
+        int256 convertedNumerator = newFromInt256(numerator);
+        int256 convertedDenominator = newFromInt256(denominator);
+        return divide(convertedNumerator, convertedDenominator);
     }
 
     /**
@@ -78,39 +59,38 @@ library FixidityLib {
      * equivalent to multiplying by 10^6.
      */
     function newFromUint256Fraction(
-        Fixidity storage fixidity, 
         uint256 numerator, 
         uint256 denominator
         )
         public
-        view
+        pure
         returns (int256)
     {
         assert(numerator < uint256(max_int256));   // Cannot process numbers above 57896044618658097711785492504343953926633.
         assert(denominator < uint256(max_int256)); // Cannot process numbers above 57896044618658097711785492504343953926633.
-        int256 convertedNumerator = newFromUint256(fixidity, numerator);
-        int256 convertedDenominator = newFromUint256(fixidity, denominator);
-        return divide(fixidity, convertedNumerator, convertedDenominator);
+        int256 convertedNumerator = newFromUint256(numerator);
+        int256 convertedDenominator = newFromUint256(denominator);
+        return divide(convertedNumerator, convertedDenominator);
     }
 
     /**
      * @dev Converts an int256 to the closest multiple of 10^36.
      */
-    function round(Fixidity storage fixidity, int256 v) public view returns (int256) {
-        return round_off(fixidity, v, fixidity.digits);
+    function round(int256 v) public pure returns (int256) {
+        return round_off(v);
     }
 
     /**
      * @dev Converts an int256 to the greatest multiple of 10^36 less than or equal to.
      */
-    function floor(Fixidity storage fixidity, int256 v) public view returns (int256) {
-        return (v / fixidity.fixed_1) * fixidity.fixed_1;
+    function floor(int256 v) public pure returns (int256) {
+        return (v / fixed_1) * fixed_1;
     }
 
     /**
      * @dev Converts to positive if negative
      */
-    function abs(Fixidity storage  /*fixidity*/, int256 x) public pure returns (int256) {
+    function abs(int256 x) public pure returns (int256) {
         if(x < 0) return -x;
         else return x;
     }
@@ -118,37 +98,37 @@ library FixidityLib {
     /**
      * @dev a*b
      */
-    function multiply(Fixidity storage fixidity, int256 a, int256 b) public view returns (int256) {
+    function multiply(int256 a, int256 b) public pure returns (int256) {
         if(a == 0 || b == 0) return 0;
-        if(b == fixidity.fixed_1) return a;
-        if(a == fixidity.fixed_1) return b;
+        if(b == fixed_1) return a;
+        if(a == fixed_1) return b;
 
-        int256 x1 = a / fixidity.fixed_1;
-        int256 x2 = a - fixidity.fixed_1 * x1;
-        int256 y1 = b / fixidity.fixed_1;
-        int256 y2 = b - fixidity.fixed_1 * y1;
-        int256 result = fixidity.fixed_1 * x1 * y1 + x1 * y2 + x2 * y1 + x2 * y2 / fixidity.fixed_1;
+        int256 x1 = a / fixed_1;
+        int256 x2 = a - fixed_1 * x1;
+        int256 y1 = b / fixed_1;
+        int256 y2 = b - fixed_1 * y1;
+        int256 result = fixed_1 * x1 * y1 + x1 * y2 + x2 * y1 + x2 * y2 / fixed_1;
         
-        if(abs(fixidity, a) < fixidity.fixed_1) assert(result < b); // these can never overflow, really
-        if(abs(fixidity, b) < fixidity.fixed_1) assert(result < a); // these can never overflow, really
-        if(abs(fixidity, a) > fixidity.fixed_1 && abs(fixidity, b) > fixidity.fixed_1) // Can this overflow twice?
-            assert(result > fixidity.fixed_1 && result > fixidity.fixed_1);
+        if(abs(a) < fixed_1) assert(result < b); // these can never overflow, really
+        if(abs(b) < fixed_1) assert(result < a); // these can never overflow, really
+        if(abs(a) > fixed_1 && abs(b) > fixed_1) // Can this overflow twice?
+            assert(result > fixed_1 && result > fixed_1);
         return result;
     }
 
     /**
      * @dev a/b
      */
-    function divide(Fixidity storage fixidity, int256 a, int256 b) public view returns (int256) {
-        if(b == fixidity.fixed_1) return a;
+    function divide(int256 a, int256 b) public pure returns (int256) {
+        if(b == fixed_1) return a;
         assert(b != 0);
-        return multiply(fixidity, a, reciprocal(fixidity, b));
+        return multiply(a, reciprocal(b));
     }
 
     /**
      * @dev a+b
      */
-    function add(Fixidity storage /*fixidity*/, int256 a, int256 b) public pure returns (int256) {
+    function add(int256 a, int256 b) public pure returns (int256) {
         int256 t = a + b;
         assert(t - a == b);
         return t;
@@ -157,7 +137,7 @@ library FixidityLib {
     /**
      * @dev a-b
      */
-    function subtract(Fixidity storage /*fixidity*/, int256 a, int256 b) public pure returns (int256) {
+    function subtract(int256 a, int256 b) public pure returns (int256) {
         int256 t = a - b;
         assert(t + a == b);
         return t;
@@ -165,30 +145,22 @@ library FixidityLib {
 
     /**
      * @dev 1/a
+     * TODO: Don't understand this one.
      */
-    function reciprocal(Fixidity storage fixidity, int256 a) public view returns (int256) {
-        return round_off(fixidity, 10 * fixidity.fixed_1 * fixidity.fixed_1 / a, 1) / 10;
+    function reciprocal(int256 a) public pure returns (int256) {
+        return round_off(10 * fixed_1 * fixed_1 / a) / 10;
     }
 
-    function round_off(Fixidity storage /*fixidity*/, int256 v, uint8 digits) public pure returns (int256) {
-        int256 t = int256(uint256(10) ** uint256(digits));
+    /**
+     * @dev If v is closer to ceiling(v) than to floor(v) then return ceiling(v). Should be round_up()
+     */
+    function round_off(int256 v) public pure returns (int256) {
         int8 sign = 1;
         if(v < 0) {
             sign = -1;
-            v = 0 - v;
+            v = abs(v);
         }
-        if(v % t >= t / 2) v = v + t - v % t;
+        if(v % fixed_1 >= fixed_1 / 2) v = v + fixed_1 - v % fixed_1;
         return v * sign;
-    }
-
-    function round_to(Fixidity storage fixidity, int256 v, uint8 digits) public view returns (int256) {
-        assert(digits < fixidity.digits);
-        return round_off(fixidity, v, fixidity.digits - digits);
-    }
-
-    function trunc_digits(Fixidity storage fixidity, int256 v, uint8 digits) public pure returns (int256) {
-        if(digits <= 0) return v;
-        return round_off(fixidity, v, digits)
-            / int256(uint256(10) ** uint256(digits));
     }
 }
--- contracts/LogarithmLib.sol
+++ contracts/LogarithmLib.sol
@@ -4,57 +4,51 @@ import "./FixidityLib.sol";
 
 library LogarithmLib {
 
-    using FixidityLib for FixidityLib.Fixidity;
+    int256 constant public fixed_log_e_1_5 =     405465108108164381978013115464349137;    
+    int256 constant public fixed_log_e_10 =     2302585092994045684017991454684364208;
 
-    uint8 constant public longer_digits = 36;
-    int256 constant public longer_fixed_log_e_1_5 =     405465108108164381978013115464349137;    
-    int256 constant public longer_fixed_1 =            1000000000000000000000000000000000000;    
-    int256 constant public longer_fixed_log_e_10 =     2302585092994045684017991454684364208;
-
-    function log_e(FixidityLib.Fixidity storage fixidity, int256 v) public view returns (int256) {
+    function log_e(int256 v) public view returns (int256) {
         assert(v > 0);
         int256 r = 0;
-        uint8 extra_digits = longer_digits - fixidity.digits;
-        int256 t = int256(uint256(10) ** uint256(extra_digits));
-        while(v <= fixidity.fixed_1 / 10) {
+        while(v <= FixidityLib.fixed_1 / 10) {
             v = v * 10;
-            r -= longer_fixed_log_e_10;
+            r -= fixed_log_e_10;
         }
-        while(v >= 10 * fixidity.fixed_1) {
+        while(v >= 10 * FixidityLib.fixed_1) {
             v = v / 10;
-            r += longer_fixed_log_e_10;
+            r += fixed_log_e_10;
         }
-        while(v < fixidity.fixed_1) {
-            v = fixidity.multiply(v, fixidity.fixed_e);
-            r -= longer_fixed_1;
+        while(v < FixidityLib.fixed_1) {
+            v = FixidityLib.multiply(v, FixidityLib.fixed_e);
+            r -= FixidityLib.fixed1;
         }
-        while(v > fixidity.fixed_e) {
-            v = fixidity.divide(v, fixidity.fixed_e);
-            r += longer_fixed_1;
+        while(v > FixidityLib.fixed_e) {
+            v = FixidityLib.divide(v, FixidityLib.fixed_e);
+            r += FixidityLib.fixed1;
         }
-        if(v == fixidity.fixed_1) {
-            return FixidityLib.round_off(fixidity, r, extra_digits) / t;
+        if(v == FixidityLib.fixed_1) {
+            return r;
         }
-        if(v == fixidity.fixed_e) {
-            return fixidity.fixed_1 + FixidityLib.round_off(fixidity, r, extra_digits) / t;
+        if(v == FixidityLib.fixed_e) {
+            return FixidityLib.fixed_1 + r;
         }
-        v *= t;
-        v = v - 3 * longer_fixed_1 / 2;
-        r = r + longer_fixed_log_e_1_5;
-        int256 m = longer_fixed_1 * v / (v + 3 * longer_fixed_1);
+
+        v = v - 3 * FixidityLib.fixed1 / 2;
+        r = r + fixed_log_e_1_5;
+        int256 m = FixidityLib.fixed1 * v / (v + 3 * FixidityLib.fixed1);
         r = r + 2 * m;
-        int256 m_2 = m * m / longer_fixed_1;
+        int256 m_2 = m * m / FixidityLib.fixed1;
         uint8 i = 3;
         while(true) {
-            m = m * m_2 / longer_fixed_1;
+            m = m * m_2 / FixidityLib.fixed1;
             r = r + 2 * m / int256(i);
             i += 2;
-            if(i >= 3 + 2 * fixidity.digits) break;
+            if(i >= 3 + 2 * FixidityLib.digits) break;
         }
-        return FixidityLib.round_off(fixidity, r, extra_digits) / t;
+        return r;
     }
 
-    function log_any(FixidityLib.Fixidity storage fixidity, int256 base, int256 v) public view returns (int256) {
-        return fixidity.divide(log_e(fixidity, v), log_e(fixidity, base));
+    function log_any(int256 base, int256 v) public view returns (int256) {
+        return FixidityLib.divide(log_e(v), log_e(base));
     }
 }
