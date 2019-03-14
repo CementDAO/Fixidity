16ec239322131957b1bd8ac98875efc1ab0cdc02
Bernardo Vieira <bernardo5@live.com.pt>
2019-03-14 19:33:09 +0000

    removed ununsed methods in utils.js

--- test/utils.js
+++ test/utils.js
@@ -22,7 +22,7 @@ const itShouldThrow = (reason, fun, expectedMessage) => {
                 error.message,
                 /invalid JUMP|invalid opcode|out of gas|The contract code couldn't be stored, please check your gas amount/,
             );
-        // An expected exception message was passed - match it.
+            // An expected exception message was passed - match it.
         } else if (error.message.length > 0) {
             // Get the error message from require method within the contract
             const errorReason = error.message.match('Reason given: (.*)\\.');
@@ -39,7 +39,7 @@ const itShouldThrow = (reason, fun, expectedMessage) => {
                     'threw the wrong exception type',
                 );
             }
-        // In case that nothing matches!
+            // In case that nothing matches!
         } else {
             assert.ok(false, `something went wrong with asserts. Given error ${error}`);
         }
@@ -51,51 +51,7 @@ const tokenNumber = (decimals, tokens) => new BigNumber(10)
     .multipliedBy(tokens)
     .toString(10);
 
-// Utils.js for Gadi's tests
-function forInstance(cb, digits) {
-	return artifacts.require("TestContract").deployed()
-		.then(instance => instance.init(digits).then(() => cb(instance)));
-}
-
-function expectToThrow(cb) {
-	var flag = false;
-	return cb()
-	.catch(() => { flag = true; return assert(true); } )
-	.then(() => { if(!flag) return assert(false); });
-}
-
-function toFixed(a, digits) {
-	return a * Math.pow(10, digits);
-}
-
-function compareFixedNumbers(a, b, digits, precision) {
-	var t = Math.pow(10, (digits - precision));
-	a = Math.round(a / t);
-	b = Math.round(b / t);
-	assert.equal(a, b);
-}
-
-function testArray(msg, data, digits, precision, cb) {
-	var pa = data.map(t => {
-		if(typeof(t[0]) == 'number') t[0] = toFixed(t[0], digits);
-		else if(Array.isArray(t[0])) t[0] = t[0].map(tt => toFixed(tt, digits));
-		it(msg + t[0], () => {
-			return artifacts.require("TestContract").deployed()
-			.then(instance => {
-				return instance.init(digits)
-				.then(() => cb(instance, t[0]))
-				.then(result => compareFixedNumbers(result.valueOf(), t[1], digits, precision));
-			});
-		});
-	});
-	return Promise.all(pa);
-};
-
 module.exports = {
     itShouldThrow,
-	tokenNumber,
-	forInstance, 
-	expectToThrow, 
-	toFixed, 
-	testArray
+    tokenNumber
 };
