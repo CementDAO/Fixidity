3547a290825d7e255ca2a4226b3ace5301f0f262
Bernardo Vieira <bernardo5@live.com.pt>
2019-04-23 12:59:03 +0100

    update dependecies and truffle-config

--- truffle-config.js
+++ truffle-config.js
@@ -1,9 +1,25 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const fs = require('fs');
+
+const infuraKey = 'your-key';
+const mnemonic = ''; // fs.readFileSync('.secret').toString().trim();
+
 module.exports = {
     networks: {
         development: {
             host: '127.0.0.1',
             port: 8545,
+            gas: 6721975,
+            gasPrice: 20000000000,
             network_id: '*',
+        },
+        ropsten: {
+            provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/${infuraKey}`),
+            network_id: 3,
+            gas: 5500000,
+            confirmations: 2,
+            timeoutBlocks: 200,
+            skipDryRun: true,
         },
     },
 
@@ -21,7 +37,13 @@ module.exports = {
 
     compilers: {
         solc: {
-            version: '0.5.0',
+            version: '0.5.2',
+            settings: {
+                optimizer: {
+                    enabled: true,
+                    runs: 200,
+                },
+            },
         },
     },
 };
