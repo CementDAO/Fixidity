3f210b3f6606206878b6868289f1fc3980de71fa
Alberto Cuesta Cañada <mail@albertocuesta.es>
2020-02-21 17:15:15 +0000

    Added safe conversion functions.

--- contracts/FixidityLib.sol
+++ contracts/FixidityLib.sol
@@ -440,4 +440,25 @@ library FixidityLib {
         assert(y <= maxFixedDivisor());
         return multiply(x, reciprocal(y));
     }
+
+    function safeIntToUint(int256 x) internal pure returns(uint256) {
+        require(
+            x >= 0,
+            "Cannot cast negative signed integer to unsigned integer."
+        );
+        return uint256(x);
+    }
+
+    /**
+     * @notice Safe casting from uint256 to int256
+     * @param x uint256 to cast
+     * @return casted int256
+     */
+    function safeUintToInt(uint256 x) internal pure returns(int256) {
+        require(
+            x <= safeIntToUint(maxInt256()),
+            "Cannot cast overflowing unsigned integer to signed integer."
+        );
+        return int256(x);
+    }
 }
